3c27e0340efd45d7c2e99d2ee8394bd428cadaaf
Moo <mamiedean@yahoo.com>
2017-01-11 21:07:25 -0500

    adding widget

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying the 404 page
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+<section class="error-page">
+    <h1>Page not found.
+    </noscript><br>I'm SORRY!!!</h1>
+</section>
+
+<?php get_footer(); ?>
+<!-- .404-page -->

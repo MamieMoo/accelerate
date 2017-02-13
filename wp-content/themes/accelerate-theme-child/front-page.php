ae7f65164916cabc48d7c2dc83fecc3576ee26e6
Moo <mamiedean@yahoo.com>
2017-02-13 14:40:42 -0500

    organizing

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -56,14 +56,16 @@ get_header(); ?>
 					<h2><?php the_title(); ?></h2>
 			        <?php the_excerpt(); ?>
 			        <a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
-  <?php endwhile; ?>
-<?php wp_reset_query(); ?>
+							<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+									<div id="secondary" class="widget-area" role="complementary">
+										<?php dynamic_sidebar( 'sidebar-2' ); ?>
+									</div>
+								<?php endif; ?>
+  			<?php endwhile; ?>
+		<?php wp_reset_query(); ?>
+		</div>
 	</div>
 </section>
-<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
-<div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-2' ); ?>
-</div>
-<?php endif; ?>
+
 <?php get_footer(); ?>
 <!-- .home-page -->
--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -24,6 +24,21 @@
  * @subpackage Accelerate Marketing
  * @since Accelerate Marketing 1.0
  */
+ function accelerate_theme_child_widget_init() {
+
+ 	register_sidebar( array(
+ 	    'name' =>__( 'Homepage sidebar', 'accelerate-theme-child'),
+ 	    'id' => 'sidebar-2',
+ 	    'description' => __( 'Appears on the static front page template', 'accelerate-theme-child' ),
+ 	    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+ 	    'after_widget' => '</aside>',
+ 	    'before_title' => '<h3 class="widget-title">',
+ 	    'after_title' => '</h3>',
+ 	) );
+
+ }
+ add_action( 'widgets_init', 'accelerate_theme_child_widget_init' );
+
  function create_custom_post_types() {
     register_post_type( 'case_studies',
         array(
@@ -49,17 +64,3 @@
     );
 }
 add_action( 'init', 'create_custom_post_types' );
-function accelerate_theme_child_widget_init() {
-
-	register_sidebar( array(
-	    'name' =>__( 'Homepage sidebar', 'accelerate-theme-child'),
-	    'id' => 'sidebar-2',
-	    'description' => __( 'Appears on the static front page template', 'accelerate-theme-child' ),
-	    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-	    'after_widget' => '</aside>',
-	    'before_title' => '<h3 class="widget-title">',
-	    'after_title' => '</h3>',
-	) );
-
-}
-add_action( 'widgets_init', 'accelerate_theme_child_widget_init' );
